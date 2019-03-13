fcfe78d80c18dce40df65d8f127973ab96b7b8c4
Ariel Lorenzo <ariellorenzo08@gmail.com>
2019-03-12 20:11:31 -0400

    Starting to add jQuery functionality

--- scripts/site.js
+++ scripts/site.js
@@ -1,1 +1,7 @@
 // Calculator Functionality
+$(document).ready(function(){
+   $(".ops").click(function() {
+      if($(".screen").text() === "0") $(".screen").val($(this).text());
+      else $(".screen").val($(".screen").text() + $(this).text());
+   });
+});
